ebb4323fc769fb4f925b1e97d2f6acfa1a569118
Jari Ylimäinen <jari@tooltip.fi>
2018-01-07 12:56:28 +0200

    Add error handling

--- datasource/jenergia-scrape/src/main.js
+++ datasource/jenergia-scrape/src/main.js
@@ -7,10 +7,12 @@ import { getLatestTimestamp, saveDataPoints } from './database';
 exports.handler = async function(event, context, callback) {
   logger.info('jenergia-scrape started');
 
-  const latest = await getLatestTimestamp();
-  const data = fetchData().then((data) => {
-  	const consumption = parseData(data);
+  const data = fetchData();
 
+  data.then(async (data) => {
+  	const consumption = parseData(data);
+    const latest = await getLatestTimestamp();
+    
     logger.info(consumption.length, 'data points fetched');
     logger.debug('last saved data point is', latest.toISOString());
 
@@ -25,8 +27,18 @@ exports.handler = async function(event, context, callback) {
         logger.info(consumption.length, 'data points saved');
         callback(null, { saved: consumption.length });
       });
+
+      save.catch((error) => {
+        logger.error('Save failed:', error);
+        callback(error, null);
+      });
     } else {
       callback(null, { saved: 0 });
     }
+  });
+
+  data.catch((error) => {
+    logger.error('Data fetching failed:', error);
+    callback(error, null);
   });
 }
